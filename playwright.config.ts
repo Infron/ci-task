02ccc50bd8412a3b9710000d149f21a7580fa3b9
Infron <Infron30@gmail.com>
2023-07-12 16:37:42 +0300

    refactor: delete comments from playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,25 +1,12 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 import { devices } from "@playwright/test";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 const config: PlaywrightTestConfig = {
   testDir: "./e2e",
 
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
     timeout: 5000,
   },
   /* Run tests in files in parallel */
